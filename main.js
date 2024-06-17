5bd072bdf55cc98cbdbdebab0e8a2cc97c4ec654
Melinacarraro <melinacarraro@outlook.com>
2024-06-17 17:37:47 -0300

    validación formulario v1

--- main.js
+++ main.js
@@ -11,3 +11,21 @@ function scrollToSection() {
     section.scrollIntoView({ behavior: "smooth" });
   }
   
+
+/*Validaciones de formulario*/
+
+
+let nombreInput = document.getElementById("nombreInput");
+
+
+
+// Vamos a tener hasta el 17 hasta las 12hs para poder entregar el proyecto grupal
+
+// individual = justificar lo que hice en base a las ideas de nilsen 
+
+
+// tipogragia
+// mapa del sitio 
+// colores 
+
+
